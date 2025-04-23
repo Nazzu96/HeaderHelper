98ee00fab24d54ef0632a6f54dc158f583b2fac0
Nazzu96 <Nazzu96@bk.ru>
2025-04-23 21:30:38 +0300

    Добавил кнопку и две подписи

--- Header/ViewModels/MainWindowViewModel.cs
+++ Header/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,38 @@
-﻿using Avalonia.Interactivity;
+﻿using System;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace Header.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
     public string Greeting { get; } = "Welcome to Avalonia!";
+    
+    private string _groupName = "ИСП-IV-03";
+    private string _lastChange = "last change: "+DateTime.Today.ToString("dd.MM.yyyy");
+    
+    private string GroupName
+    {
+        get => _groupName;
+        set
+        {
+            _groupName = value;
+            OnPropertyChanged(nameof(GroupName));
+        }
+    }
+
+    private string LastChanged
+    {
+        get => _lastChange;
+        set
+        {
+            _lastChange = value;
+            OnPropertyChanged(nameof(LastChanged));
+        }
+    }
+
+    private void DownloadButton_Click(object? sender, RoutedEventArgs e)
+    {
+        
+    }
 }
--- Header/obj/Debug/net9.0/Header.AssemblyInfo.cs
+++ Header/obj/Debug/net9.0/Header.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Header")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bbf99a36da15841753caa2497eea042cf58bc5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Header")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Header")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
