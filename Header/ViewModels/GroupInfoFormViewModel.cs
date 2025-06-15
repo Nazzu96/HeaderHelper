5e5ccfb05fca43094be953525bb0649c837f3184
Nazzu96 <Nazzu96@bk.ru>
2025-06-15 09:33:49 +0300

    Надо

--- Header/ViewModels/GroupInfoFormViewModel.cs
+++ Header/ViewModels/GroupInfoFormViewModel.cs
@@ -1,22 +1,25 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data.Common;
+using System.Net;
 using Avalonia.Controls;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Header.Utils;
 using Header.Utils.Models;
-using OfficeOpenXml.ConditionalFormatting.Contracts;
+using Newtonsoft.Json;
 
 namespace Header.ViewModels;
 
 public partial class GroupInfoFormViewModel : ObservableObject
 {
+    
     // TODO: Сделать возможным заполнение на первый семестр и второй
     [ObservableProperty] private TypeOfGrid _typeGrid = TypeOfGrid.Student;
-    [ObservableProperty] private string _fullNameOrDiscipline = string.Empty;
+    [ObservableProperty] private string _fullNameOrDiscipline;
 
-    [ObservableProperty] private string _studentName = string.Empty;
+    [ObservableProperty] private string _studentName;
     [ObservableProperty] private string _disciplineName = string.Empty;
 
     [ObservableProperty] private string _groupDirection = string.Empty;
@@ -25,15 +28,18 @@ public partial class GroupInfoFormViewModel : ObservableObject
 
     public string GroupName;
     public ObservableCollection<Disciplines> Disc { get; set; } = Global.Disc;
-    public ObservableCollection<Students> Std { get; set; } = Global.Std;
+    //public ObservableCollection<Students> Std { get; set; } = Global.Std;
+    public ObservableCollection<Students> Students { get; } = new();
     
-        [RelayCommand]
-    private void AddButton()
+    [RelayCommand]
+    private void Add()
     {
-        switch (TypeGrid)
+        if (string.IsNullOrWhiteSpace(_fullNameOrDiscipline))
+        /*switch (TypeGrid)
         {
             case TypeOfGrid.Student:
             {
+                
                 //FullNameOrDiscipline;
 
                 break;
@@ -44,9 +50,9 @@ public partial class GroupInfoFormViewModel : ObservableObject
                 //FullNameOrDiscipline;
                 break;
             }
-        }
+        }*/
     }
-
+    
     // private string WatermarkText()
     // {
     //     
